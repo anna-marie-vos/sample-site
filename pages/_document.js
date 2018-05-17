6c99d0921ffc4838b3f5c57aa1fad8dafd957617
anna-marie-vos <amvos@outlook.com>
2018-05-18 09:30:59 +1200

    starting styling

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import css from '../components/styles/style'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <title>Sample Page</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css"/>
+          <style>{css}</style>
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
