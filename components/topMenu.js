1d375b8d9afaedcb9def0bf65d715bbd44387908
anna-marie-vos <amvos@outlook.com>
2018-05-19 19:31:59 +1200

    top menu name changed

--- components/topMenu.js
+++ components/topMenu.js
@@ -4,7 +4,7 @@ class topMenu extends React.Component {
   render () {
     return (
       <div className="grid-container-one">
-        <h1 className="grid-item">Keepsite Risk Register</h1>
+        <h1 className="grid-item">Company Risk Register</h1>
       </div>
     )
   }
